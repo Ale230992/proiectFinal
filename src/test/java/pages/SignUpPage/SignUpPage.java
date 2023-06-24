6287659491b82298708681d3fddf2a05e71d2ff4
Ale230992 <alexandranicoara92@yahoo.ro>
2023-06-24 19:00:42 +0300

    Added changes 24.06

--- src/test/java/pages/SignUpPage/SignUpPage.java
+++ src/test/java/pages/SignUpPage/SignUpPage.java
@@ -24,6 +24,19 @@ public class SignUpPage extends BasePage {
     private By adresaDeEmailField = By.xpath("//input[@type='email']");
     private By continuaButton = By.xpath("//button[@class='Button js-authSubmitButton']");
     private By suntDeAcordButton = By.xpath("//button[@type='submit']");
+    private By alegeOParolaField = By.xpath("//form[@id='AuthForm']/div[4]/input[@type='password']");
+    private By continua1Button = By.xpath("//button[@class='Button js-authSubmitButton']");
+    private By numeSiprenumeleField = By.xpath("//form[@id='AuthForm']/div[5]/input[@type='text']");
+    private By telefonField = By.xpath("//form[@id='AuthForm']/div[5]/input[@type='tel']");
+    private By continua2Button = By.xpath("//button[@class='Button js-authSubmitButton']");
+    private By adresaField = By.xpath("//form[@id='AuthForm']/div[6]/input[1][@type='text']");
+    private By localitateField = By.xpath("//form[@id='AuthForm']/div[6]/input[2][@type='text']");
+    private By judetField = By.xpath("//form[@id='AuthForm']/div[6]/input[3][@type='text']");
+    private By taraField = By.xpath("//form[@id='AuthForm']/div[6]/input[4][@type='text']");
+    private By puncteDeReperField = By.xpath("//form[@id='AuthForm']/div[6]/input[5][@type='text']");
+    private By continua3Button = By.xpath("//button[@class='Button js-authSubmitButton']");
+    private By persoanaFizicaRadioButton = By.xpath("//form[@id='AuthForm']/div[7]/div[2]/div[1]/div[1]/div[1]['RadioButton-icon']");
+    private By creeazaContulButton = By.xpath("//form[@id='AuthForm']/div[12]/Div[1]//button[@class='Button js-authSubmitButton']");
 
     public boolean isAutentificareButtonDisplayed() {
         LOG.info("Verify if 'Autentificare' button is displayed");
@@ -50,5 +63,72 @@ public class SignUpPage extends BasePage {
         driver.findElement(suntDeAcordButton).click();
     }
 
+    public void typeInAlegeOparolaField(String password) {
+        LOG.info("Typing password in 'Alege o Parola' field");
+        driver.findElement(alegeOParolaField).sendKeys(password);
+    }
 
+    public void clickContinua1Button() {
+        LOG.info("Click 'Continua' button");
+        driver.findElement(continua1Button).click();
+    }
+
+    public void typeInNumeSiPrenumeleField(String numesiprenumele) {
+        LOG.info("Typing nume and prenume in 'Nume si Prenumele' field");
+        driver.findElement(numeSiprenumeleField).sendKeys(numesiprenumele);
+    }
+
+    public void typeInTelefonField(String telefon) {
+        LOG.info("Typing telefon in 'Telefon' field");
+        driver.findElement(telefonField).sendKeys(telefon);
+    }
+
+    public void clickContinua2Button() {
+        LOG.info("Click 'Continua' button");
+        driver.findElement(continua2Button).click();
+    }
+
+    public void typeInAdresaField(String adresa) {
+        LOG.info("Typing adresa in 'Adresa' field");
+        driver.findElement(adresaField).sendKeys(adresa);
+    }
+
+    public void typeInLocalitateField(String localitate) {
+        LOG.info("Typing localitate in 'Localitate' field");
+        driver.findElement(localitateField).sendKeys(localitate);
+    }
+
+    public void typeInJudetField(String judet) {
+        LOG.info("Typing judet in 'Judet' field");
+        driver.findElement(judetField).sendKeys(judet);
+    }
+
+    public boolean isTaraFieldButtonTypedIn() {
+        LOG.info("Verify if 'Tara' field is typed in");
+        return driver.findElement(taraField).isDisplayed();
+    }
+
+    public void typeInPunctDeReperField(String punctdereper) {
+        LOG.info("Typing punct de reper in 'Punct de reper' field");
+        driver.findElement(puncteDeReperField).sendKeys(punctdereper);
+    }
+
+    public void clickContinua3Button() {
+        LOG.info("Click 'Continua' button");
+        driver.findElement(continua3Button).click();
+    }
+
+    public void clickPersoanaFizicaRadioButton() {
+        LOG.info("Click 'Persoana fizica' radio button");
+        driver.findElement(persoanaFizicaRadioButton).click();
+    }
+
+    public void clickCreeazaContulButton() {
+        LOG.info("Click 'Creeaza contul' button");
+        driver.findElement(creeazaContulButton).click();
+    }
 }
+
+
+
+
--- src/test/java/tests/SignUpTest/SignUpTest.java
+++ src/test/java/tests/SignUpTest/SignUpTest.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.SignUpPage.SignUpPage;
 import tests.BaseTest;
 
 import static pages.BasePage.sleep;
@@ -15,26 +14,86 @@ public class SignUpTest extends BaseTest {
     @Test
     public void SignUp() {
         String email = "ale.nyx23@gmail.com";
+        String password = "AleNyx2309";
+        String numesiprenumele = "Nicoara Alexandra";
+        String telefon = "0756 906 275";
+        String adresa = "Str. Luptei, nr.2";
+        String localitate = "Sibiu";
+        String judet = "Sibiu";
+        String punctdereper = "Elmina";
+
 
         LOG.info("Check 'Autentificare' button");
         Assert.assertTrue(signUpPage.isAutentificareButtonDisplayed(), "Button is not displayed");
-        sleep (3000);
+        sleep(3000);
 
         LOG.info("Click 'Autentificare' button");
         signUpPage.clickAutentificareButton();
-        sleep (3000);
+        sleep(3000);
 
         LOG.info("Complete the 'Adresa de Email' field");
         signUpPage.typeInAdresaDeEmailField(email);
-        sleep (3000);
+        sleep(3000);
 
         LOG.info("Click 'Continua' button");
         signUpPage.clickContinuaButton();
-        sleep (3000);
+        sleep(3000);
 
         LOG.info("Click 'Sunt de acord' button");
         signUpPage.clickSuntDeAcordButton();
-        sleep (3000);
+        sleep(3000);
+
+        LOG.info("Complete the 'Alege o Parola' field");
+        signUpPage.typeInAlegeOparolaField(password);
+        sleep(3000);
+
+        LOG.info("Click 'Continua' button");
+        signUpPage.clickContinua1Button();
+        sleep(3000);
+
+        LOG.info("Complete the 'Nume si Prenumele' field");
+        signUpPage.typeInNumeSiPrenumeleField(numesiprenumele);
+        sleep(3000);
+
+        LOG.info("Complete the 'Telefon' field");
+        signUpPage.typeInTelefonField(telefon);
+        sleep(3000);
+
+        LOG.info("Click 'Continua' button");
+        signUpPage.clickContinua2Button();
+        sleep(3000);
+
+        LOG.info("Complete the 'Adresa' field");
+        signUpPage.typeInAdresaField(adresa);
+        sleep(3000);
+
+        LOG.info("Complete the 'Localitate' field");
+        signUpPage.typeInLocalitateField(localitate);
+        sleep(3000);
+
+        LOG.info("Complete the 'Judet' field");
+        signUpPage.typeInJudetField(judet);
+        sleep(3000);
+
+        LOG.info("Check 'Tara' field is typed in");
+        Assert.assertTrue(signUpPage.isTaraFieldButtonTypedIn(), "Field is not typed in");
+        sleep(3000);
+
+        LOG.info("Complete the 'Punct de reper' field");
+        signUpPage.typeInPunctDeReperField(punctdereper);
+        sleep(3000);
+
+        LOG.info("Click 'Continua' button");
+        signUpPage.clickContinua3Button();
+        sleep(3000);
+
+        LOG.info("Click 'Persoana fizica' radio button");
+        signUpPage.clickPersoanaFizicaRadioButton();
+        sleep(3000);
+
+        LOG.info("Click 'Creeaza contul' button");
+        signUpPage.clickCreeazaContulButton();
+        sleep(3000);
 
 
     }
